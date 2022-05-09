b5d04dc522a198ec42c521ae7dc4f4a074ea565e
TheStram <90732077+TheStram@users.noreply.github.com>
2022-05-09 08:49:11 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/styles/index.css">
+    <title>Tifon</title>
+</head>
+<body>
+<div id="particles-js"></div>
+<center><h2 id="Tifon"><hr>©Tifon<hr></h2></center>
+<header> 
+<center><div id="bienvenido">
+<br>
+<br>
+<h1>Bienvenido</h1><br>
+<p id="parrafo00">Por temas de seguridad para ver <br>
+    el contenido de esta pagina nesesitas<br>
+    iniciar secion o registrarte.<br><br>
+    Puedes hacerlo con los botones de abajo.<br>
+</p>
+<br>
+<br>
+<br>
+</div></center></header>
+<center>
+<div id="forms"><hr><a href="login.php" id="iniciasecion">Inicia Secion</a>ó<a href="singup.php" id="registrate">Registrate</a>
+    <hr></div></center>
+    <center><p id="parrafo01">Marca registrada ©copyright Tifon Enterprise</p></center>
+<script src="assets/scrips/particles.min.js"></script>
+<script src="assets/scrips/particles.index01.js"></script>
+</body>
+</html>
